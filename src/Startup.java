50e44989ab69f98b522339f088854d797d674b70
Torleif Hensvold <torleif.hensvold@outlook.com>
2018-12-21 14:51:37 +0100

    Initial startup-commit.
    Added the IDE files to the VCS.
    Added the main startup JavaFX file with no content.

---
+++ src/Startup.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Torleif on 21.12.2018.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Startup extends Application
+{
+	
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage)
+	{
+		
+	}
+}
